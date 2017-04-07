0086e28dc9fa36f756b416e16c6cd2dc72dde982
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-04-08 00:18:17 +0300

    version 2.0.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 To run program vcard_read.py on Ubuntu:
 
-1) make sure that python3 and all needed packages are installed: os, sys, base64, PIL
+1) make sure that python3 and all needed packages are installed: io, os, sys, base64, shutil, re, PIL
 2) change mode of the file to executable:  chmod +x vcard_read.py
 3) copy vcard_read.py into Nautilus scripts folder: ~/.local/share/nautilus/scripts
 4) once you have done all this, the script will be accessible from the scripts sub-menu of the right click menu in Nautilus.
--- read_vcard.py
+++ read_vcard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
+# Version 2.0.0 April, 8 2017
 # "THE BEER-WARE LICENSE" (Revision 42):
 # Dmitrii Sokolov <sokolovdp@gmail.com> wrote this file. As long as you retain
 # this notice you can do whatever you want with this stuff. If we meet some day,
@@ -13,13 +14,17 @@
 import os
 import sys
 import base64
+import io
+import shutil
+import re
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+
 temp_thumb_file = 'thumbnail_tmp.jpg'
-stand_pars = ['FN', 'TITLE', 'ORG', 'ADR', 'TEL', 'EMAIL', 'URL', 'PHOTO']
-max_param_length = 24
+stand_pars = ['N', 'FN', 'TITLE', 'ORG', 'ADR', 'TEL', 'EMAIL', 'URL']  # PHOTO processed separately
+EOL = '&&&'
 X = 154
 Y = 20
 OFF = 20
@@ -32,121 +37,107 @@ font_size = 10
 font_file = "Roboto-Regular.ttf"  # can be used instead of default font
 
 
-def load_vcards(lines):  # form list of lines for each vcard, begin line excluded
-    all_cards = list()
-    card_lines = list()
-    inside_card = False
-    for line in lines:
-        if ":" in line:  # parameter line 
-            param, value = line.strip().split(':')
-            if "BEGIN" in param:
-                if not inside_card:
-                    inside_card = True
-                else:
-                    print("invalid vcard format: END line missing")
-                    return None
-            elif "END" in param:
-                if inside_card:
-                    card_lines.append(line)
-                    all_cards.append(card_lines)
-                    card_lines = list()
-                    inside_card = False
-                else:
-                    print("invalid vcard format: END line before BEGIN line")
-                    return None
+def load_vcards(filename):  # parse VCF file into list of dicts with vcard params
+    with open(filename, encoding="utf8") as f:
+        data = f.read()
+    data = data.replace('\n', EOL)
+    vcard_format = r"BEGIN:VCARD(?P<card>.*?)END:VCARD"
+    p_vcard = re.compile(vcard_format)
+    photo_format = r"PHOTO;(?P<photo>.*?)/9k=" + EOL
+    p_photo = re.compile(photo_format)
+    param_format = r"(?P<param>[A-Z]+);(?P<value>.*?)" + EOL
+    p_param = re.compile(param_format)
+    cards_list = list()
+
+    for match1 in p_vcard.finditer(data):
+        vcard_params = dict()
+        vcard_text = match1.group('card')
+        match2 = re.search(p_photo, vcard_text)
+        if match2:  # there is a photo image in the vcard
+            span = match2.span()
+            photo_value = match2.group('photo').replace(EOL, '')
+            photo_code = '%s%s' % (photo_value.split(':')[1], '/9k=')
+            try:
+                image_bytes = base64.b64decode(photo_code)
+            except:
+                print('error in base64 encoding, image data ignored')
             else:
-                card_lines.append(line)
-        elif inside_card:
-            card_lines.append(line)
-        else:
-            continue
-    return all_cards
-
-
-def decode_data(index, lines, value):
-    photo_data = value
-    for line in lines[index + 1:]:
-        if "END:VCARD" in line:
-            break
-        else:
-            photo_data += line.strip()
-    # ignore parameters of encoding in pars list
-    try:
-        imgdata = base64.b64decode(photo_data)
-    except:
-        print('error in base64 encoding, data ignored')
-        return ''
-    else:
-        with open(temp_thumb_file, 'wb') as f:
-            f.write(imgdata)
-        return temp_thumb_file
-
-
-def get_photo(card_lines):
-    photo_file = ''
-    for index, line in enumerate(card_lines):
-        if ":" in line:  # parameter line 
-            param, value = line.strip().split(':')
-            p, *p_var = param.split(';')
-            if p == 'PHOTO':
-                if 'http' in value.lower():
-                    # download photo from inet
-                    break
+                try:
+                    image = Image.open(io.BytesIO(image_bytes))
+                except:
+                    print('error in image format, image ignored')
                 else:
-                    # data is in the list, so we have to decode them
-                    photo_file = decode_data(index, card_lines, value)
-                    break
-    return photo_file
+                    vcard_params["PHOTO"] = image
+            vcard_text = vcard_text[:span[0]]
+            tail = vcard_text[span[1]:]
+            if len(tail):
+                vcard_text += tail
+        n_given = False
+        fn_given = False
+        for match3 in p_param.finditer(vcard_text):  # find other parameters
+            param = match3.group('param').strip()
+            value = match3.group('value').split(':')[1].replace(';', ' ').strip()
+            if param == 'N':
+                n_given = True
+            if param == 'FN':
+                fn_given = True
+            if (param in stand_pars) and (param not in vcard_params.keys()):
+                vcard_params[param] = value
+        if vcard_params:
+            if (not n_given) and (not fn_given):
+                print("no name parameters (N, FN) in data, VCARD ignored")
+                continue
+            if n_given and fn_given:  # there must only FN parameter in vcard data
+                del vcard_params['N']
+            elif n_given:
+                vcard_params['FN'] = vcard_params['N']
+                del vcard_params['N']
+            cards_list.append(vcard_params)
+        else:
+            print("no valid parameters in data, VCARD ignored")
+    print("loaded vcards:", len(cards_list))  # debug
+    return cards_list
 
 
 def create_thumbnail(card_info):
-    background = Image.new('RGBA', thumb_size, background_color)
-    draw = ImageDraw.Draw(background)
     # font = ImageFont.truetype(font_file, font_size)
     font = ImageFont.load_default()
-    params_list = list(card_info.keys())
-    if ('PHOTO' in params_list) and (card_info['PHOTO'] != ''):
-        img = Image.open(card_info['PHOTO'], 'r')
-        small_img = img.resize(small_size)
-        background.paste(small_img, pic_offset)
-    offset = Y
-    for par in params_list:
-        if par != 'PHOTO':
-            draw.text((X, offset), par + ': ' + card_info[par], text_color, font=font)
-            offset += OFF
-    background.save((card_info['FN'].lower() + '.png').replace(' ', '_'))
-
-
-def card_to_thumbnail(card_lines):
-    card_data = dict()
-    if 'PHOTO' in stand_pars:  # process PHOTO par if present standard parameters
-        result = get_photo(card_lines)
-        if result != '':
-            card_data['PHOTO'] = result
-    for line in card_lines:  # process others parameters
-        if ":" in line:  # parameter line 
-            param, value = line.strip().split(':')
-            p1, *_ = param.split(';')
-            if (p1 in stand_pars) and (p1 not in card_data):
-                temp = " ".join(value.split(';')).strip()
-                if len(temp) > max_param_length:  # cut long string
-                    temp = temp[:max_param_length]
-                card_data[p1] = temp
-    create_thumbnail(card_data)
+    background = Image.new('RGBA', thumb_size, background_color)
+    draw = ImageDraw.Draw(background)
+    y = Y
+    x = X
+    if 'PHOTO' not in card_info.keys():
+        x = X - 100
+    for param in list(card_info.keys()):
+        if param == 'PHOTO':
+            img = card_info['PHOTO']
+            small_img = img.resize(small_size)
+            background.paste(small_img, pic_offset)
+        else:
+            draw.text((x, y), param + ': ' + card_info[param], text_color, font=font)
+            y += OFF
+    thumb_file = (card_info['FN'].lower() + '.png').replace(' ', '_')
+    if os.path.isfile(thumb_file):
+        print('duplicated vcard name and thumb file name:', thumb_file)
+    try:
+        background.save((card_info['FN'].lower() + '.png').replace(' ', '_'))
+    except IOError:
+        print("IO error during writing thumb file:", thumb_file)
 
 
 def main(vcard_file):
-    with open(vcard_file, encoding="utf8") as f:
-        data = f.readlines()
-    dirname = vcard_file.lower().split('.')[0] + ".thumbs"
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    os.chdir(dirname)
-    list_of_cards = load_vcards(data)
+    list_of_cards = load_vcards(vcard_file)
     if list_of_cards:
-        for card in list_of_cards:
-            card_to_thumbnail(card)
-        os.remove(temp_thumb_file)  # remove temporal jpg file
+        dirname = vcard_file.lower().split('.')[0] + ".thumbs"
+        shutil.rmtree(dirname, ignore_errors=True)  # remove old directory
+        try:
+            os.makedirs(dirname)  # create new directory
+        except:
+            print("access error: can't create directory:", dirname)
+        else:
+            os.chdir(dirname)
+            for card in list_of_cards:
+                create_thumbnail(card)
 
 
 if __name__ == '__main__':
